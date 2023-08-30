052a3b94b624ed5d9d352759901c4a7c8ed61349
yuta <kuromame.guri@gmail.com>
2023-08-30 22:22:51 +0900

    敵が一定のエリア内にランダムで出現する プレイヤーの装備が上書きして保存される

--- src/main/java/plugin/enemydown/command/EnemyDownCommand.java
+++ src/main/java/plugin/enemydown/command/EnemyDownCommand.java
@@ -1,13 +1,17 @@
 package plugin.enemydown.command;
 
+import java.util.List;
 import java.util.SplittableRandom;
 import org.bukkit.Location;
+import org.bukkit.Material;
 import org.bukkit.World;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.EntityType;
 import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.PlayerInventory;
 
 public class EnemyDownCommand implements CommandExecutor {
 
@@ -15,18 +19,61 @@ public class EnemyDownCommand implements CommandExecutor {
   @Override
   public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
     if(sender instanceof Player player){
-      player.setHealth(20);
-      player.setFoodLevel(20);
-
       World world = player.getWorld();
-      Location playerLocation = player.getLocation();
-      double x = playerLocation.getX();
-      double y = playerLocation.getY();
-      double z = playerLocation.getZ();
 
-      int random = new SplittableRandom().nextInt(10)-20;
-      world.spawnEntity(new Location(world,x + random,y,z + random), EntityType.ZOMBIE);
+      initPlayerStatus(player);
+
+      world.spawnEntity(getEnemySpawnLocation(player, world), getEnemy());
     }
     return false;
+  }
+
+  /**
+   * ゲームを始める前にプレイヤーんの状態を設定する。
+   * 体力と空腹度を最大にして、装備はネザライト一式になる。
+   *
+   * @param player  コマンドを実行したプレイヤー
+   */
+  private void initPlayerStatus(Player player) {
+    player.setHealth(20);
+    player.setFoodLevel(20);
+    PlayerInventory playerInventory = player.getInventory();
+    playerInventory.setHelmet(new ItemStack(Material.NETHERITE_HELMET));
+    playerInventory.setChestplate(new ItemStack(Material.NETHERITE_CHESTPLATE));
+    playerInventory.setLeggings(new ItemStack(Material.NETHERITE_LEGGINGS));
+    playerInventory.setBoots(new ItemStack(Material.NETHERITE_BOOTS));
+    playerInventory.setItemInMainHand(new ItemStack(Material.NETHERITE_SWORD));
+  }
+
+  /**
+   * 敵の出現場所を取得します。
+   * 出現エリアはX軸とZ軸は自分の位置からプラス、ランダムで-10から9の値が設定されます。
+   * Y軸はプレイヤーと同じ位置になります。
+   *
+   * @param player　コマンドを実行したプレイヤー
+   * @param world　コマンドを実行したプレイヤーが所属するワールド
+   * @return  敵の出現場所
+   */
+  private Location getEnemySpawnLocation(Player player, World world) {
+    Location playerLocation = player.getLocation();
+    int randomX = new SplittableRandom().nextInt(10) - 20;
+    int randomZ = new SplittableRandom().nextInt(10) - 20;
+
+    double x = playerLocation.getX() + randomX;
+    double y = playerLocation.getY();
+    double z = playerLocation.getZ() + randomZ;
+
+    Location enemySpawnLocation = new Location(world, x,y,z);
+    return enemySpawnLocation;
+  }
+
+  /**
+   * ランダムで敵を抽選して、その結果を取得します。
+   *
+   * @return  敵
+   */
+  private EntityType getEnemy() {
+    List<EntityType> enemyList =List.of(EntityType.ZOMBIE,EntityType.SKELETON);
+    return enemyList.get(new SplittableRandom().nextInt(2));
   }
 }
