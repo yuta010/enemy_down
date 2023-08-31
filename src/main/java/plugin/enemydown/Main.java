6f7202ca4f55e3cbbc2baa71b28c7ba467f2fbf8
yuta <kuromame.guri@gmail.com>
2023-08-31 21:14:14 +0900

    敵を倒すと点数が入るように実装

--- src/main/java/plugin/enemydown/Main.java
+++ src/main/java/plugin/enemydown/Main.java
@@ -1,15 +1,14 @@
 package plugin.enemydown;
 
 import org.bukkit.Bukkit;
-import org.bukkit.event.Listener;
 import org.bukkit.plugin.java.JavaPlugin;
 import plugin.enemydown.command.EnemyDownCommand;
 
-public final class Main extends JavaPlugin implements Listener {
+public final class Main extends JavaPlugin  {
     @Override
     public void onEnable() {
-        Bukkit.getPluginManager().registerEvents(this, this);
-        getCommand("enemyDown").setExecutor(new EnemyDownCommand());
+        EnemyDownCommand enemyDownCommand = new EnemyDownCommand();
+        Bukkit.getPluginManager().registerEvents(enemyDownCommand,this);
+        getCommand("enemyDown").setExecutor(enemyDownCommand);
     }
-
 }
